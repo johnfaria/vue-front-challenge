431295298d210958cd6f4a2fa12cb9e8b50ad1f2
John Faria <johnwbf@hotmail.com>
2020-11-05 02:47:18 -0300

    component: criado Footer.vue

---
+++ src/components/Footer.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-footer dark padless>
+    <v-card class="flex" flat tile>
+      <v-card-title class="teal">
+        <strong class="subheading">Visite este projeto no GitHub</strong>
+
+        <v-spacer></v-spacer>
+
+        <v-btn v-for="icon in icons" :key="icon" class="mx-4" dark icon>
+          <v-icon size="24px">
+            {{ icon }}
+          </v-icon>
+        </v-btn>
+      </v-card-title>
+
+      <v-card-text class="py-2 white--text text-center">
+        {{ new Date().getFullYear() }} — <strong>Made by John</strong>
+      </v-card-text>
+    </v-card>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    icons: ['mdi-github']
+  })
+}
+</script>
