39c4e490f00c550bacb4f95975031e9b37b346f1
John Faria <johnwbf@hotmail.com>
2020-11-05 06:56:14 -0300

    feat: criado componente PageNotFound

---
+++ src/views/PageNotFound.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-container fill-height>
+    <v-row justify="center">
+      <v-alert prominent type="error">
+        <v-row align="center">
+          <v-col class="grow">
+            Page Not Found
+          </v-col>
+        </v-row>
+      </v-alert>
+    </v-row>
+  </v-container>
+</template>
