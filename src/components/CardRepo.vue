d09b54ba7477237d1dd5d1d5139019c1ee4ce7fe
John Faria <johnwbf@hotmail.com>
2020-11-05 06:54:03 -0300

    feat: criado componente CardRepo

---
+++ src/components/CardRepo.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-card class="mx-auto" color="dark" dark>
+    <v-card-title>
+      <v-icon large left>
+        mdi-github
+      </v-icon>
+      <span class="title font-weight-light">{{ repository.name }}</span>
+    </v-card-title>
+
+    <v-card-text class="headline font-weight-bold">
+      {{ repository.description }}
+    </v-card-text>
+
+    <v-card-actions>
+      <v-list-item class="grow">
+        <v-list-item-content>
+          <v-list-item-title>{{ repository.language }}</v-list-item-title>
+        </v-list-item-content>
+
+        <v-row align="center" justify="end">
+          <v-icon class="mr-1">
+            mdi-star
+          </v-icon>
+          <span class="subheading mr-2">{{ repository.stargazers_count }}</span>
+        </v-row>
+      </v-list-item>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'CardRepo',
+  props: {
+    repository: Object
+  },
+  data: () => ({
+    //
+  })
+}
+</script>
