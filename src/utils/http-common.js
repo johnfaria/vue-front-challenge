7a8ce263ee41be44c02fee00a3f7e6cf91902015
John Faria <johnwbf@hotmail.com>
2020-11-06 16:00:45 -0300

    fix: fix cors headers

--- src/utils/http-common.js
+++ src/utils/http-common.js
@@ -1,5 +1,10 @@
 import axios from 'axios'
 
 export const HTTP = axios.create({
-  baseURL: `https://myapp-ci.azurewebsites.net`
+  baseURL: `https://myapp-ci.azurewebsites.net`,
+  headers: {
+    'Access-Control-Allow-Origin': 'https://myappci-front.azurewebsites.net/',
+    'Access-Control-Request-Method': 'GET, POST',
+    'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token, auth-token'
+  }
 })
